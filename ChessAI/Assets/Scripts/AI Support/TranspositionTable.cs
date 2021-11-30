c52d4a26c3298e13388c8a55b60575ee3ea01f40
mnokno <kubaalfaproCode@gmail.com>
2021-11-30 15:51:26 +0000

    Implemented supports structures for TranspositionTable class

--- ChessAI/Assets/Scripts/AI Support/TranspositionTable.cs
+++ ChessAI/Assets/Scripts/AI Support/TranspositionTable.cs
@@ -23,7 +23,34 @@ namespace Chess.EngineUtility
 
         #endregion
 
-        #region Structures
+        #region Structures and enums
+
+        public struct HashEntry
+        {
+            // Struct variables
+            public readonly long key;
+            public readonly byte depth;
+            public readonly NodeType nodeType;
+            public readonly int score;
+            public readonly ushort move;
+
+            // Struct constructor
+            public HashEntry(long key, byte depth, NodeType nodeType, int score, ushort move)
+            {
+                this.key = key;
+                this.depth = depth;
+                this.nodeType = nodeType;
+                this.score = score;
+                this.move = move;
+            }
+        } 
+
+        public enum NodeType
+        {
+            exact = 0, // PV-node aka exact
+            alphaCutoff = 1, // All-node aka upper bound
+            betaCutoff = 2 // Cut-node aka lower bound
+        }
 
         #endregion
     }
