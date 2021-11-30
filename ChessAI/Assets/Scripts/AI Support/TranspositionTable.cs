dd8a2d853d382fd7f0a09f006d7d9420b85fae5c
mnokno <kubaalfaproCode@gmail.com>
2021-11-30 14:53:58 +0000

    Added skeleton code for TranspositionTable class

---
+++ ChessAI/Assets/Scripts/AI Support/TranspositionTable.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chess.EngineUtility
+{
+    public class TranspositionTable
+    {
+        #region Class variables
+
+        #endregion
+
+        #region Constructor
+
+        public TranspositionTable()
+        {
+
+        }
+
+        #endregion
+
+        #region Utility
+
+        #endregion
+
+        #region Structures
+
+        #endregion
+    }
+}
+
