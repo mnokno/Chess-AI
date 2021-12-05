597b9b358b11a11cd3d6db3dd7ac75349e3e941d
mnokno <kubaalfaproCode@gmail.com>
2021-12-05 16:50:02 +0000

    Added opening book

---
+++ ChessAI/Assets/Scripts/AI Support/OpeningBook.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chess.EngineUtility
+{
+    public static class OpeningBook
+    {
+        #region Class variables
+
+        // Directory of the opening book
+        private static string openingBookDirectory = Application.streamingAssetsPath + "/OpeningBook.txt";
+        // Array containing all the games from the opening book
+
+        #endregion
+
+        #region Class Constructor
+
+        static OpeningBook()
+        {
+
+        }
+
+        #endregion
+
+        #region Class Utility
+
+        #endregion
+    }
+}
