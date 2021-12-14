261dbc42b5751f20d31947fa624bcc6fe14d3cd4
mnokno <kubaalfaproCode@gmail.com>
2021-12-14 22:25:04 +0000

    Implementation
    Progress in opening book implementation

--- ChessAI/Assets/Scripts/AI Support/OpeningBook.cs
+++ ChessAI/Assets/Scripts/AI Support/OpeningBook.cs
@@ -10,6 +10,7 @@ namespace Chess.EngineUtility
 
         // Directory of the opening book
         private static string openingBookDirectory = Application.streamingAssetsPath + "/OpeningBook.txt";
+        private static Dictionary<ulong, Entry> book = new Dictionary<ulong, Entry>();
         // Array containing all the games from the opening book
 
         #endregion
@@ -18,12 +19,39 @@ namespace Chess.EngineUtility
 
         static OpeningBook()
         {
+            string[] games = System.IO.File.ReadAllText(openingBookDirectory).Split('\n');
+            foreach (string game in games)
+            {
+                Debug.Log(game);
+            }
+        }
 
+        public static void RunMe()
+        {
+            return;
         }
 
         #endregion
 
         #region Class Utility
+
+        #endregion
+
+        #region Structures
+
+        public struct Entry
+        {
+            public ulong key;
+            public ushort move;
+            public ushort count;
+
+            public Entry(ulong key, ushort move)
+            {
+                this.key = key;
+                this.move = move;
+                this.count = 1;
+            }
+        }
 
         #endregion
     }
