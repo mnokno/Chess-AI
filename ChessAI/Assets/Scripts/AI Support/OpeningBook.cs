c0d24a7d1a2e37606ede579cdd637739d328fd25
mnokno <62951079+mnokno@users.noreply.github.com>
2021-12-24 15:32:10 +0000

    Finished opening book class

--- ChessAI/Assets/Scripts/AI Support/OpeningBook.cs
+++ ChessAI/Assets/Scripts/AI Support/OpeningBook.cs
@@ -10,7 +10,7 @@ namespace Chess.EngineUtility
 
         // Directory of the opening book
         private static string openingBookDirectory = Application.streamingAssetsPath + "/OpeningBook.txt";
-        private static Dictionary<ulong, Entry> book = new Dictionary<ulong, Entry>();
+        private static Dictionary<ulong, List<Entry>> book = new Dictionary<ulong, List<Entry>>();
         // Array containing all the games from the opening book
 
         #endregion
@@ -20,17 +20,43 @@ namespace Chess.EngineUtility
         static OpeningBook()
         {
             string[] games = System.IO.File.ReadAllText(openingBookDirectory).Split('\n');
-            int gameIndex = 1; // Debug
             foreach (string game in games)
             {
-                Debug.Log($"Game Index: {gameIndex} ------------------------------------------------------------------------------------------------------------------");
                 string[] moves = game.Replace(" 1/2-1/2", "").Replace(" 0-1", "").Replace(" 1-0", "").Replace("\r", "").Split(" ");
                 Position playBackPosition = new Position();
                 foreach (string move in moves)
                 {
                     playBackPosition.MakeMove(Move.ConvertPGNToUshort(move.Replace(" ", ""), playBackPosition));
+                    if (book.ContainsKey(playBackPosition.zobristKey))
+                    {
+                        // Gets list of all entries attached to this key
+                        List<Entry> entries = book[playBackPosition.zobristKey];
+                        ushort currentlyEvaluatedMove = Move.ConvertPGNToUshort(move, playBackPosition);
+
+                        // Cheks if the currently evaluated move allready exists in the move list
+                        bool entryAllreadyExists = false;
+                        for (int i = 0; i < entries.Count; i++)
+                        {
+                            if (entries[i].move == currentlyEvaluatedMove)
+                            {
+                                entries[i].IncreaseCount();
+                                entryAllreadyExists = true;
+                                break;
+                            }
+                        }
+                        if (!entryAllreadyExists)
+                        {
+                            Entry newEntry = new Entry(playBackPosition.zobristKey, Move.ConvertPGNToUshort(move, playBackPosition));
+                            entries.Add(newEntry);
+                        }
+                    }
+                    else
+                    {
+                        // Adds this move to the book and creates a list of moves for this key
+                        Entry newEntry = new Entry(playBackPosition.zobristKey, Move.ConvertPGNToUshort(move, playBackPosition));
+                        book[playBackPosition.zobristKey] = new List<Entry> { newEntry };
+                    }
                 }
-                gameIndex ++; // Debug
             }
         }
 
@@ -43,6 +69,25 @@ namespace Chess.EngineUtility
 
         #region Class Utility
 
+        public static string GetStringBook()
+        {
+            // Converts the book from a dictiorary to a formated string
+            string formatedBook = "";
+            
+            // Foreach position
+            foreach(ulong key in book.Keys)
+            {
+                string bookLine = key.ToString() + " :";
+                foreach(Entry entry in book[key])
+                {
+                    bookLine += " " + entry.move + "-" + entry.count;
+                }
+                formatedBook += bookLine + "\n";
+            }
+
+            // Returns the formated book
+            return formatedBook;
+        }
         #endregion
 
         #region Structures
@@ -58,6 +103,11 @@ namespace Chess.EngineUtility
                 this.key = key;
                 this.move = move;
                 this.count = 1;
+            }
+
+            public void IncreaseCount()
+            {
+                count++;
             }
         }
 
