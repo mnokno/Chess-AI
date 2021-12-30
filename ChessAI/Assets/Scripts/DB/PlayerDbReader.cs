498713e600c59832b19135f402c676c6f2571f8c
mnokno <62951079+mnokno@users.noreply.github.com>
2021-12-30 15:54:42 +0000

    Created skeleton code for player DB

---
+++ ChessAI/Assets/Scripts/DB/PlayerDbReader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chess.DB
+{
+    public class PlayerDbReader
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
