e4d8427f6c4825743c33739765092f1791c70d37
mnokno <62951079+mnokno@users.noreply.github.com>
2021-12-31 13:56:49 +0000

    implemented a popup message

---
+++ ChessAI/Assets/Scripts/UI/PopUpMessage.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Chess.UI
+{
+    public class PopUpMessage : MonoBehaviour
+    {
+        // Class variables
+        public Animator animator;
+        public TMPro.TextMeshProUGUI messageText;
+        private Action action;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            animator.SetTrigger("Show");
+        }
+
+        // Called when the background button is pressed
+        public void BackGroundBtn()
+        {
+            animator.SetTrigger("Pouls");
+        }
+
+        // Called when the ok button is pressed
+        public void OkBtn()
+        {
+            animator.SetTrigger("Hide");
+            if (action != null)
+            {
+                action();
+            }
+        }
+
+        // Shows the popup message
+        public void Show()
+        {
+            animator.SetTrigger("Show");
+        }
+
+        // Sets action that is executed when the ok button is pressed
+        public void SetAction(Action action)
+        {
+            this.action = action;
+        }
+
+        // Sets message in the massage box
+        public void SetMessage(string message)
+        {
+            messageText.text = message;
+        }
+    }
+}
