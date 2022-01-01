d2ea43c33dda50beb462911d2cb1d5433d101bce
mnokno <62951079+mnokno@users.noreply.github.com>
2022-01-01 19:28:55 +0000

    Created a game creation scene

--- ChessAI/Assets/Scripts/UI/HomeUI.cs
+++ ChessAI/Assets/Scripts/UI/HomeUI.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HomeUI : MonoBehaviour
+namespace Chess.UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HomeUI : MonoBehaviour
     {
-        
-    }
+        private void Start()
+        {
+            Debug.Log(PlayerPrefs.GetString("username"));
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void StartNewGameBtn()
+        {
+            FindObjectOfType<SceneLoader>().LoadScene("GameCreationScene");
+        }
+
+        public void LoadGameBtn()
+        {
+            FindObjectOfType<SceneLoader>().LoadScene("SavedGameSelectionScene");
+        }
+
+        public void ReviewGameBtn()
+        {
+            FindObjectOfType<SceneLoader>().LoadScene("ReviewGameSelectionScene");
+        }
+
+        public void LogOutBtn()
+        {
+            PlayerPrefs.SetString("username", "");
+            FindObjectOfType<SceneLoader>().LoadScene("StartScene");
+        }
+
+        public void QuitBtn()
+        {
+            Application.Quit();
+        }
     }
 }
--- ChessAI/Assets/Scripts/UI/StartUI.cs
+++ ChessAI/Assets/Scripts/UI/StartUI.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Chess.DB;
 
 namespace Chess.UI
 {
     public class StartUI : MonoBehaviour
     {
+        private void Awake()
+        {
+            string username = PlayerPrefs.GetString("username");
+            if (username != "")
+            {
+                PlayerDbReader reader = new PlayerDbReader();
+                reader.OpenDB();
+                PlayerDb.PlayerRecord playerRecord = reader.TryGetRecord(username);
+                reader.CloseDB();
+                if (playerRecord.isValid)
+                {
+                    UnityEngine.SceneManagement.SceneManager.LoadScene("HomeScene");
+                }
+            }
+        }
+
         public void SelectProfileBtn()
         {
             FindObjectOfType<SceneLoader>().LoadScene("ProfileSelectionScene");
